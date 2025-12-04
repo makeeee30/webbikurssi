1dbbd9979c03a79ea54e88f189cb5c3e4ee14fe2
maria.halla-aho <maria.hallaaho22@gmail.com>
2025-12-04 18:36:20 +0200

    lisätty uusi task

---
+++ task-j/index.js
@@ -0,0 +1,22 @@
+// index.js
+// Author: Maria Halla-aho
+// Date: 2025-12-03
+
+function showPage(pageId) {
+  // Piilota kaikki osiot
+  document.querySelectorAll("section").forEach(sec => {
+    sec.style.display = "none";
+  });
+
+  // Näytä vain haluttu osio
+  const page = document.getElementById(pageId);
+  if (page) {
+    page.style.display = "block";
+  }
+}
+
+// Kun sivu latautuu, näytetään etusivu
+
+window.onload = function() {
+  showPage("etusivu"); 
+};
